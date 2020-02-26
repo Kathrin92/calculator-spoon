f87c14d2645433e717c2d3480298271999972b1c
Kathrin92 <kathrinulmer@gmx.de>
2020-02-26 11:38:08 +0100

    Add javascript and style

--- src/calculator.js
+++ src/calculator.js
@@ -1,1 +1,19 @@
+"use strict";
 
+const calculatorOutput = document.querySelector(".calculator__output");
+const calculatorInput = document.querySelector(".calculator__input");
+
+function add(numberOne, numberTwo) {
+  return numberOne + numberTwo;
+}
+function subtract(numberOne, numberTwo) {
+  return numberOne - numberTwo;
+}
+function divide(numberOne, numberTwo) {
+  return numberOne / numberTwo;
+}
+function multiply(numberOne, numberTwo) {
+  return numberOne * numberTwo;
+}
+const numberOne = Number(calculatorInput.innerTexr);
+calculatorOutput.value = add(calculatorInput.innerText, 33);
