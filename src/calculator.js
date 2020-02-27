ef86c84a3128f4edc96fb548341f04cba5cd7a16
Kathrin92 <kathrinulmer@gmx.de>
2020-02-27 13:12:45 +0100

    Calculate based on operator

--- src/calculator.js
+++ src/calculator.js
@@ -1,4 +1,4 @@
-import { add } from "./math.js";
+import { calculate } from "./math.js";
 // import math from "./math.js";
 
 const calculatorOutput = document.querySelector(".calculator__output");
@@ -9,10 +9,11 @@ const calculatorOperators = document.querySelectorAll(".calculator__operator");
 
 let numberOne = 0;
 let numberTwo = 0;
+let operator = "";
 
 function handleResultClick() {
   numberTwo = Number(calculatorOutput.value);
-  calculatorOutput.value = add(numberOne, numberTwo);
+  calculatorOutput.value = calculate(numberOne, numberTwo, operator);
   console.log(
     "handleResultClick",
     numberOne,
@@ -55,6 +56,7 @@ calculatorInputs.forEach(addInputEventListener);
 function addOperatorEventListener(calculatorOperator) {
   function handleCalculatorOperatorClick() {
     numberOne = Number(calculatorOutput.value);
+    operator = calculatorOperator.innerText;
     clear();
     console.log(
       "handleCalculatorOperatorClick",
--- src/math.js
+++ src/math.js
@@ -14,6 +14,23 @@ export function multiply(numberOne, numberTwo) {
   return numberOne * numberTwo;
 }
 
+export function calculate(numberOne, numberTwo, operator) {
+  if (operator === "+") {
+    return add(numberOne, numberTwo);
+  }
+  if (operator === "-") {
+    return subtract(numberOne, numberTwo);
+  }
+
+  if (operator === "*") {
+    return multiply(numberOne, numberTwo);
+  }
+
+  if (operator === "/") {
+    return divide(numberOne, numberTwo);
+  }
+}
+
 // export default {
 //   add,
 //   subtract,
