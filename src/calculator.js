109fcd4a7e9a7f27d09b3ca73df9aa89c6dab9e4
Kathrin92 <kathrinulmer@gmx.de>
2020-02-27 11:07:12 +0100

    Create math modules

--- src/calculator.js
+++ src/calculator.js
@@ -1,19 +1,68 @@
-"use strict";
+import { add, subtract, divide, multiply } from "./math.js";
+// import math from "./math.js";
 
 const calculatorOutput = document.querySelector(".calculator__output");
-const calculatorInput = document.querySelector(".calculator__input");
+const calculatorInputs = document.querySelectorAll(".calculator__input");
+const calculatorResult = document.querySelector(".calculator__result");
+const calculatorClear = document.querySelector(".calculator__clear");
+const calculatorOperators = document.querySelectorAll(".calculator__operator");
 
-function add(numberOne, numberTwo) {
-  return numberOne + numberTwo;
+let numberOne = 0;
+let numberTwo = 0;
+
+function handleResultClick() {
+  numberTwo = Number(calculatorOutput.value);
+  calculatorOutput.value = add(numberOne, numberTwo);
+  console.log(
+    "handleResultClick",
+    numberOne,
+    numberTwo,
+    calculatorOutput.value
+  );
 }
-function subtract(numberOne, numberTwo) {
-  return numberOne - numberTwo;
+
+calculatorResult.addEventListener("click", handleResultClick);
+
+function clear() {
+  calculatorOutput.value = "";
 }
-function divide(numberOne, numberTwo) {
-  return numberOne / numberTwo;
+
+calculatorClear.addEventListener("click", clear);
+
+// function handleInputClick() {
+//   console.log("Handle input click");
+// }
+// calculatorInputs[0].addEventListener("click", handleInputClick);
+// calculatorInputs[1].addEventListener("click", handleInputClick);
+
+function addInputEventListener(calculatorInput) {
+  function handleCalculatorInputClick() {
+    // calculatorOutput.value = calculatorOutput.value + calculatorInput.innerText;
+    calculatorOutput.value += calculatorInput.innerText;
+    console.log(
+      "handleCalculatorInputClick",
+      numberOne,
+      numberTwo,
+      calculatorOutput.value
+    );
+  }
+
+  calculatorInput.addEventListener("click", handleCalculatorInputClick);
 }
-function multiply(numberOne, numberTwo) {
-  return numberOne * numberTwo;
+
+calculatorInputs.forEach(addInputEventListener);
+
+function addOperatorEventListener(calculatorOperator) {
+  function handleCalculatorOperatorClick() {
+    numberOne = Number(calculatorOutput.value);
+    clear();
+    console.log(
+      "handleCalculatorOperatorClick",
+      numberOne,
+      numberTwo,
+      calculatorOutput.value
+    );
+  }
+  calculatorOperator.addEventListener("click", handleCalculatorOperatorClick);
 }
-const numberOne = Number(calculatorInput.innerTexr);
-calculatorOutput.value = add(calculatorInput.innerText, 33);
+calculatorOperators.forEach(addOperatorEventListener);
